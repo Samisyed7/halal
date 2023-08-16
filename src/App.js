9e50d95da1773ce497571243051964b125216283
MohdAhteshamHasan <mohdahtesham2002@gmail.com>
2023-08-16 14:30:42 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1>zayn</h1>
-     <h1>Shaikh Maksud laal hai</h1>
-     <p>MERN DEVLOPER</p>
-     <h1>ahtesham</h1>
-     <h2>bablu</h2>
+     
 
     </div>
   );
